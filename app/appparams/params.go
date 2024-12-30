c189d2408563e9015ae81a5b0d18e23bdc4ef3e9
Robert Zaremba <robert@zaremba.ch>
2024-12-30 14:03:07 +0100

    feat: add NTIV bank Metadata
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/appparams/params.go
+++ app/appparams/params.go
@@ -2,17 +2,43 @@ package appparams
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	banktypes "cosmossdk.io/x/bank/types"
 )
 
 const (
-	// Name defines the application name.
-	Name = "gonative"
+	// AppName defines the application name.
+	AppName = "gonative"
 
 	// BondDenom defines the native staking token denomination.
 	// NOTE: it is used by IBC, and must not change to avoid token migration in all IBC chains.
 	BondDenom = "untiv"
+	// DisplayDenom defines the name, symbol, and display value of the NTIV token.
+	DisplayDenom = "NTIV"
 )
 
 func init() {
 	sdk.DefaultBondDenom = BondDenom
+}
+
+// NtivTokenMetadata creates bank Metadata for the NTIV token
+func NtivTokenMetadata() banktypes.Metadata {
+	return banktypes.Metadata{
+		Description: "The operational token of the Native blockchain.",
+		Base:        BondDenom, // NOTE: must not change
+		Name:        DisplayDenom,
+		Display:     DisplayDenom,
+		Symbol:      DisplayDenom,
+		DenomUnits: []*banktypes.DenomUnit{
+			{
+				Denom:    BondDenom,
+				Exponent: 0,
+				Aliases:  []string{"microntiv"},
+			}, {
+				Denom:    DisplayDenom,
+				Exponent: 6,
+				Aliases:  []string{},
+			},
+		},
+	}
 }
